fd6369379a5274134fb3ed2b531ea60fdf5a67ce
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-06-05 11:19:08 -0400

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Kiho Okazawa on 2023-06-05.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
