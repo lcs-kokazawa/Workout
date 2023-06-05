5b913433fc1004cafdea98898d1caed4afca1804
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-06-05 11:22:35 -0400

    Completed the initial project organization

---
+++ Workout/Views/WorkoutListView.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutListView.swift
+//  Workout
+//
+//  Created by Kiho Okazawa on 2023-06-05.
+//
+
+import SwiftUI
+
+struct WorkoutListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutListView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutListView()
+    }
+}
