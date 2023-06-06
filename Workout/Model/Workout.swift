4f805a3b18c735c0b7603c5019ece10fcddd57ac
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-06-06 14:01:39 -0400

    Added the Blackbird package and the boilerplate code needed to copy the database file into our app's sandbox on a device

---
+++ Workout/Model/Workout.swift
@@ -0,0 +1,11 @@
+//
+//  Workout.swift
+//  Workout
+//
+//  Created by Kiho Okazawa on 2023-06-05.
+//
+
+import Blackbird
+import Foundation
+
+
--- Workout/Views/AddWorkoutView.swift
+++ Workout/Views/AddWorkoutView.swift
@@ -6,15 +6,55 @@
 //
 
 import SwiftUI
+import Blackbird
 
 struct AddWorkoutView: View {
     //MARK: Stored properties
-    
+    //Access the conncetion to the database (neededto add a new record)
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    //Holds details for the new movie
+    @State var date = ""
+    @State var duration = 20
+    @State var sumosquats = 0
+    @State var bulgariansplit = 0
     //MARK: Computed properties
     var body: some View {
         NavigationView{
+            ScrollView {
+                VStack {
+                    HStack{
+                        Button(action: {
+                            sumosquats += 1
+                        }, label: {
+                            Image(systemName: "figure.cross.training")
+                            Text("sumosquats")
+                        })
+                        .buttonStyle(.borderedProminent)
+                        .tint(.green)
+                        
+                        Text("× \(sumosquats)")
+                    }
+                    HStack{
+                        Button(action: {
+                            bulgariansplit += 1
+                        }, label: {
+                            Image(systemName: "figure.cross.training")
+                            Text("bulgariansplit")
+                        })
+                        .buttonStyle(.borderedProminent)
+                        .tint(.green)
+                        
+                        Text("× \(bulgariansplit)")
+                    }
+            }
             
+                
+            }
+            .navigationTitle("Record Workout")
+            
+          
         }
+        
         
     }
         
